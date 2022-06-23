ca8d4b1699437571e7b6e8944dceef9f3c9811ab
Oncu <oohancan@gmail.com>
2022-06-23 16:02:19 +0300

    Initial Commit

---
+++ BuildTinderApp/BuildTinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildTinderAppApp.swift
+//  BuildTinderApp
+//
+//  Created by Oncu Ohancan on 23.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct BuildTinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
