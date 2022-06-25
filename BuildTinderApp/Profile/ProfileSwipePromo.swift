ed45b5df2f0daa51b28a80d425a9f1fdf738d93c
Oncu <oohancan@gmail.com>
2022-06-25 12:19:01 +0300

    add ProfileSwipePromo

---
+++ BuildTinderApp/Profile/ProfileSwipePromo.swift
@@ -0,0 +1,69 @@
+//
+//  ProfileSwipePromo.swift
+//  BuildTinderApp
+//
+//  Created by Oncu Ohancan on 25.06.2022.
+//
+
+import SwiftUI
+
+struct ProfileSwipePromo: View {
+    
+    var action: () -> Void
+    
+    var body: some View {
+        VStack(spacing: 0){
+            TabView {
+                VStack(spacing:9) {
+                    Spacer()
+                    Text("Get Tinder Gold")
+                        .font(.system(size: 26, weight: .semibold))
+                    Text("See who Likes You & More")
+                }
+                .padding(.bottom, 64)
+                
+                VStack(spacing:9) {
+                    Spacer()
+                    Text("Get Matches Faster")
+                        .font(.system(size: 26, weight: .semibold))
+                    Text("Boost your profile once a month!")
+                }
+                .padding(.bottom, 64)
+                
+                VStack(spacing:9) {
+                    Spacer()
+                    Text("Increase Your Changes")
+                        .font(.system(size: 26, weight: .semibold))
+                    Text("Boost your profile once a month!")
+                }
+                .padding(.bottom, 64)
+            }
+            .tabViewStyle(PageTabViewStyle())
+            
+            Button(action: { action() }, label: {
+                Text("MY TINDER PLUS")
+                    .foregroundColor(.red)
+                    .font(.system(size: 18, weight: .semibold))
+                    .padding(.vertical, 20)
+                    .padding(.horizontal, 50)
+                    .background(.white)
+                    .cornerRadius(30)
+                    .shadow(color: Color.gray.opacity(0.2), radius: 10)
+            })
+            .padding(.bottom, 30)
+        }
+    }
+}
+
+struct ProfileSwipePromo_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.gray.opacity(0.2)
+                .edgesIgnoringSafeArea(.all)
+            ProfileSwipePromo {
+                print("Test")
+            }
+        }
+        .frame(height: 400)
+    }
+}
