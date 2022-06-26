5d5aec7d2343745c41d7f0fc55693a7d7cfbbea5
Oncu <oohancan@gmail.com>
2022-06-26 16:01:59 +0300

    add Message

---
+++ BuildTinderApp/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  BuildTinderApp
+//
+//  Created by Oncu Ohancan on 26.06.2022.
+//
+
+import Foundation
+
+struct Message {
+    var content: String
+    var person: String
+}
+
