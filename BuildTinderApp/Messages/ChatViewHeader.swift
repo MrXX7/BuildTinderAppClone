80fa8c42d541ce050c5523c49794313cd9f6204f
Oncu <oohancan@gmail.com>
2022-06-28 15:22:41 +0300

    add and edit ChatViewHeader

--- BuildTinderApp/Messages/ChatViewHeader.swift
+++ BuildTinderApp/Messages/ChatViewHeader.swift
@@ -8,17 +8,41 @@
 import SwiftUI
 
 struct ChatViewHeader: View {
+    @Environment(\.presentationMode) var presentationMode
     let name: String
     let imageURL: URL?
     let videoAction: () -> Void
     
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            Color.white
+                .edgesIgnoringSafeArea(.top)
+                .shadow(radius: 3)
+            HStack {
+                Button(action: { presentationMode.wrappedValue.dismiss() }, label: {
+                    Image(systemName: "chevron.left")
+                        .foregroundColor(Color.textPrimary)
+                        .font(.system(size: 28, weight: .semibold))
+                })
+                Spacer()
+                VStack (spacing: 0){
+                    RoundedImage(url: imageURL)
+                        .frame(height: 80)
+                    Text(name)
+                        .foregroundColor(Color.textSecondary)
+                    
+                        .font(.system(size: 13))
+                }
+                
+                Button(action: { videoAction() }, label: {
+                    Image(systemName: "video.fill")
+                        .font(.system(size: 20, weight: .bold))
+                })
+            }
+        }
+        .frame(height: 50)
     }
 }
-    
-    
-    
     
 struct ChatViewHeader_Previews: PreviewProvider {
     static var previews: some View {
