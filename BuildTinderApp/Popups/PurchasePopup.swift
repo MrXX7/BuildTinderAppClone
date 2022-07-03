b7b2d4eed89cf51323fed2ea67f5e7c2da95086c
Oncu <oohancan@gmail.com>
2022-07-03 14:04:15 +0300

    add purchasePopup

---
+++ BuildTinderApp/Popups/PurchasePopup.swift
@@ -0,0 +1,20 @@
+//
+//  PurchasePopup.swift
+//  BuildTinderApp
+//
+//  Created by Oncu Ohancan on 3.07.2022.
+//
+
+import SwiftUI
+
+struct PurchasePopup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PurchasePopup_Previews: PreviewProvider {
+    static var previews: some View {
+        PurchasePopup()
+    }
+}
